14fd3dd21676f64b3f9de796d6c4b14dd1b4333e
devarchi33 <devarchi33@gmail.com>
2016-05-20 02:38:33 +0900

    1. templates 폴더의 html 파일들에서 정적 자원 인식 할 수 있도록 설정.

--- src/main/java/kr/domaindriven/Application.java
+++ src/main/java/kr/domaindriven/Application.java
@@ -3,13 +3,29 @@ package kr.domaindriven;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by jerry on 2016-05-15.
  */
 @SpringBootApplication
-public class Application {
-    public static void main(String ... args){
-        ApplicationContext applicationContext = SpringApplication.run(Application.class,args);
+@EnableWebMvc
+public class Application extends WebMvcConfigurerAdapter {
+    public static void main(String... args) {
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * static folder 이하에 있는 정적자원들 인식.
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/static/**")) {
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
     }
 }
